f39fde97168c8620b3dce4898b1001df7bbf9a43
Thiccblique <104588992+Thiccblique@users.noreply.github.com>
2023-12-13 15:17:39 -0800

    Refrehend up some aseets and spawbers

--- Yuniti/Assets/Yuniti/Scripts/UI/HUD/Hud.cs
+++ Yuniti/Assets/Yuniti/Scripts/UI/HUD/Hud.cs
@@ -17,11 +17,26 @@ public class Hud : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            if (HUD != null)
+            {
+
+                Animator anim = HUD.GetComponent<Animator>();
+                if (anim != null)
+                {
+                    isOpen = anim.GetBool("open");
+
+                    anim.SetBool("open", !isOpen);
+
+                }
+            }
+        }
     }
 
     public void HUDController()
     {
+       
        
         if (HUD != null)
         {

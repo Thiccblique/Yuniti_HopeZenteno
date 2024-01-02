1c1d9299e172e3ad94a26ab11f603304a420762c
Thiccblique <104588992+Thiccblique@users.noreply.github.com>
2024-01-02 11:27:30 -0800

    Enhanced the main menu && added gamble talking audio

--- Yuniti/Assets/Yuniti/Scripts/Dialouge/DialogueManager.cs
+++ Yuniti/Assets/Yuniti/Scripts/Dialouge/DialogueManager.cs
@@ -50,6 +50,7 @@ public class DialogueManager : MonoBehaviour
         foreach (char letter in sentences.ToCharArray())
         {
             dialogueText.text += letter;
+            FindAnyObjectByType<AudioManager>().Play("Text");
             yield return new WaitForSeconds(.02f);
             yield return null;
         }

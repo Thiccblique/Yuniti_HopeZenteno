dd5c11c6036dc5d4f231859cd135c2eba3b1beff
d3trim3ntal <obsidianblackrose@gmail.com>
2024-01-06 12:53:42 -0800

    MainMenu almost done
    Only need keys index and finishing up circle wipe animation.

--- Yuniti/Assets/Yuniti/Scripts/MainMenu/ButtonManager.cs
+++ Yuniti/Assets/Yuniti/Scripts/MainMenu/ButtonManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using TMPro;
 using UnityEngine.Audio;
 using UnityEngine;
 
@@ -8,7 +9,7 @@ public class ButtonManager : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
-    public Dropdown resolutionDropdown;
+    public TMPro.TMP_Dropdown resolutionDropdown;
 
     Resolution[] resolutions;
 
@@ -20,13 +21,28 @@ public class ButtonManager : MonoBehaviour
 
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
+
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "hz";
             options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetVolume (float volume)

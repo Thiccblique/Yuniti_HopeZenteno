c333bcfbcbd13ceeecf1adf27b40f1401eb39943
JJmojica23 <jjmojica2015@gmail.com>
2024-02-17 00:54:54 -0800

    Bull Behaviour + Pathing
    I made a new BullBehaviour for the new Bull enemy prefab. The pathing is inside this behaviour and it uses the position of gameobjects inside the pathing array. Also added a new tag for them. It should work fine, but I haven't tested it much when the game runs. It also needs a spawner.

---
+++ Yuniti/Assets/Yuniti/Scripts/Enemy/BullBehaviour.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class BullBehaviour : MonoBehaviour
+{
+    public static BullBehaviour instance; // This is just used for other variables in other scripts to keep track of instance variables inside the instance of this BullBehaviour (if that makes sense).
+
+    [Header("Bull Pathing")]
+    public NavMeshAgent enemyBull; // makes a variable of type NavMeshAgent which used for the NavMesh navigating. We call it enemyBull.
+    public GameObject[] pathing; // an array of GameObjects to get the Vector3 position.
+
+    private Vector3 lastPoint; // an instance variable (Vector3) that will get the last Vector3 position for the pathing array named lastPoint.
+    private Vector3 currentAssignedPoint; // an instance variable (Vector3) that keeps track of the current Vector3 position the bull will be going towards to. Named currentAssignedPoint.
+    private int currentPathingIndex; // an instance variable (int) that keeps track of the current index of the pathing array. For example, index 0 would be the first Vector3 waypoint. Named currentPathingIndex.
+
+    private TowerHealth towerHealth;
+    private PlayerHealth playerHealth;
+    private ProjectileBehaviour projectileBehaviour;
+    private FWProjectileBehaviour fwProjectileBehaviour;
+    private WallBehavior wallBehavior;
+    public IsometricController player;
+    public OrionController orionPlayer;
+
+    [Header("Health")]
+    public float healthAmount;
+    public float maxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPoint = pathing[pathing.Length - 1].transform.position; // assigns the lastPoint to the last GameObject's Vector3 position in the pathing array.
+        currentAssignedPoint = pathing[0].transform.position; // assigns the first GameObject Vector3 position of the pathing array to currentAssignedPoint.
+        currentPathingIndex = 0; // sets currentPathingIndex to 0 since in an array this would be the first element inside it.
+        enemyBull.SetDestination(currentAssignedPoint); // sets the first destination using NavMesh to currentAssignedPoint which would be just the first Vector3 position of the first GameObject in pathing array.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyBull.SetDestination(currentAssignedPoint); // constantly updates the destination of the bull using NavMesh based on the current Vector3 point assigned to currentAssignedPoint.
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // === Bull Pathing ===
+
+        if (other.gameObject.CompareTag("BullWaypoint") && currentAssignedPoint != lastPoint) // Checks the tag of the other object that has a collider trigger to see if it is "BullWaypoint".
+        {                                                                                     // Also checks if the other waypoint isn't the last point of the pathing array to avoid an IndexOutOfBounds error.
+            currentPathingIndex++; // adds 1 to the currentPathingIndex variable
+            currentAssignedPoint = pathing[currentPathingIndex].transform.position; // assigns the new Vector3 position of the next GameObject in the pathing array using the new currentPathingIndex number.   
+        }
+
+        if (other.gameObject.CompareTag("BullWaypoint") && other.gameObject.transform.position == lastPoint) // Checks the tag of the other object that has a collider trigger to see if it is "BullWaypoint".
+        {                                                                                                    // It also checks if it is the last point in the pathing array to show that it has reached the final destination.
+            // things to do once it reaches the last point
+            Destroy(gameObject); // placeholder - it just destroys the bull
+        }
+
+        // === Bull Being Attacked ===
+
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+            projectileBehaviour = other.gameObject.GetComponent<ProjectileBehaviour>();
+            healthAmount = healthAmount - projectileBehaviour.damageAmount;
+            // healthBar.value = CalculateHealth();
+            //HitPartical();
+            //ActivateRandomParticleSystem();
+            FindAnyObjectByType<AudioManager>().Play("EnemyHit");
+            // Debug.Log("Enemy Health: " + healthAmount);
+        }
+
+        if (other.gameObject.CompareTag("FWProjectile"))
+        {
+            fwProjectileBehaviour = other.gameObject.GetComponent<FWProjectileBehaviour>();
+            healthAmount = healthAmount - fwProjectileBehaviour.damageAmount;
+            //healthBar.value = CalculateHealth();
+            //HitPartical();
+            //ActivateRandomParticleSystem();
+            FindAnyObjectByType<AudioManager>().Play("EnemyHit");
+            //Debug.Log("Enemy Health: " + healthAmount);
+        }
+
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            // wallBehavior = other.gameObject.GetComponent<WallBehavior>();
+            //healthAmount = healthAmount - wallBehavior.damageAmount;
+            healthAmount--;
+            //healthbar.value = CalculateHealth();
+            //HitPartical();
+            //ActivateRandomParticleSystem();
+            FindAnyObjectByType<AudioManager>().Play("EnemyHit");
+            // Debug.Log("Enemy Health: " + healthAmount);
+        }
+
+        if (other.gameObject.CompareTag("HitBox"))
+        {
+            healthAmount -= player.attackDamage;
+
+            //ActivateRandomParticleSystem();
+            FindAnyObjectByType<AudioManager>().Play("EnemyHit");
+
+        }
+        if (other.gameObject.CompareTag("SwordHitBox"))
+        {
+            healthAmount -= player.swordDamage;
+            //ActivateRandomParticleSystem();
+            FindAnyObjectByType<AudioManager>().Play("EnemyHit");
+        }
+    }
+}

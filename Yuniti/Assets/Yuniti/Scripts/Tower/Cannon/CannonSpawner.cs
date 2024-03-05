ca3034651bb380aa5c4b7cbd94495d7d8a604218
Thiccblique <104588992+Thiccblique@users.noreply.github.com>
2024-03-04 23:22:50 -0800

    d

--- Yuniti/Assets/Yuniti/Scripts/Tower/Cannon/CannonSpawner.cs
+++ Yuniti/Assets/Yuniti/Scripts/Tower/Cannon/CannonSpawner.cs
@@ -11,11 +11,7 @@ public class CannonSpawner : MonoBehaviour
     public GameObject bigCollier;
 
     public string tagToDestroy = "YourTagHere";
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
     // Update is called once per frame
     void Update()
